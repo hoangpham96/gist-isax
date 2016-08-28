2c4a83bcc3bdbb840e0062404561098704ea5c2b
Bryn Jeffries <bryn.jeffries@sydney.edu.au>
2016-08-28 10:19:52 +1000

    Added skeleton for basic comparison of arrays

--- gist_isax--1.0.sql
+++ gist_isax--1.0.sql
@@ -1,43 +1,54 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION gist_isax" to load this file. \quit
 
-CREATE OR REPLACE FUNCTION my_consistent(internal, data_type, smallint, oid, internal)
+CREATE FUNCTION array_dist(anyarray, anyarray)
+RETURNS real
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT;
+
+CREATE FUNCTION arrays_similar(anyarray, anyarray)
 RETURNS bool
 AS 'MODULE_PATHNAME'
 LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION my_union(internal, internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
+CREATE OPERATOR % (LEFTARG = anyarray, RIGHTARG = anyarray, PROCEDURE = arrays_similar);
 
-CREATE OR REPLACE FUNCTION my_compress(internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
+-- CREATE OR REPLACE FUNCTION my_consistent(internal, data_type, smallint, oid, internal)
+-- RETURNS bool
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION my_decompress(internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
+-- CREATE OR REPLACE FUNCTION my_union(internal, internal)
+-- RETURNS internal
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION my_penalty(internal, internal, internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
+-- CREATE OR REPLACE FUNCTION my_compress(internal)
+-- RETURNS internal
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION my_picksplit(internal, internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
+-- CREATE OR REPLACE FUNCTION my_decompress(internal)
+-- RETURNS internal
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION my_same(internal, internal, internal)
-RETURNS internal
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
+-- CREATE OR REPLACE FUNCTION my_penalty(internal, internal, internal)
+-- RETURNS internal
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
 
-CREATE OR REPLACE FUNCTION my_distance(internal, data_type, smallint, oid)
-RETURNS float8
-AS 'MODULE_PATHNAME'
-LANGUAGE C STRICT;
+-- CREATE OR REPLACE FUNCTION my_picksplit(internal, internal)
+-- RETURNS internal
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
 
+-- CREATE OR REPLACE FUNCTION my_same(internal, internal, internal)
+-- RETURNS internal
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
+
+-- CREATE OR REPLACE FUNCTION my_distance(internal, data_type, smallint, oid)
+-- RETURNS float8
+-- AS 'MODULE_PATHNAME'
+-- LANGUAGE C STRICT;
---
+++ gist_isax.h
@@ -0,0 +1,7 @@
+#ifndef __GIST_ISAX_H__
+#define __GIST_ISAX_H__
+
+#define SimilarityStratergyNumber 1
+extern double distance_threshold;
+
+#endif /* __GIST_ISAX_H__ */
