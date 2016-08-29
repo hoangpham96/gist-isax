57402e7526e71541b92a48cf579dc08aa56995dc
brynjeffries <bryn.jeffries@sydney.edu.au>
2016-08-29 13:46:54 +1000

    Added more detailed tests for time-series ops

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ OBJS = gist_isax.o
 DATA = gist_isax--1.0.sql
 DOCS = README.gist_isax
 MODULES = gist_isax
-REGRESS = gist_isax_test
+REGRESS = timeseries_ops_test
 
 PG_CONFIG = pg_config
 PGXS := $(shell $(PG_CONFIG) --pgxs)
---
+++ sql/timeseries_ops_test.sql
@@ -0,0 +1,75 @@
+CREATE EXTENSION gist_isax;
+/*
+ * GUC parameters 
+ */
+-- Default value of 0.5 should be reachable via SHOW
+SHOW gist_isax.distance_threshold;
+
+-- Shouldn't be able to set negative distances
+SET gist_isax.distance_threshold TO -0.1;
+SHOW gist_isax.distance_threshold;
+
+-- Should be able to set threshold and see the updated value
+SET gist_isax.distance_threshold TO 0.1;
+SHOW gist_isax.distance_threshold;
+
+/*
+ * array_dist function
+ */
+-- Don't allow comparisons between arrays of different lengths
+SELECT array_dist('{1.0,1.0,1.0,1.0}'::real[], '{2.0,2.0,2.0}'::real[]);
+SELECT array_dist('{1.0,1.0,1.0}'::real[], '{2.0,2.0,2.0,2.0}'::real[]);
+
+-- Comparisons involving NULLs should give result unknown 
+SELECT array_dist('{null,1.0,1.0,1.0}'::real[], '{2.0,2.0,2.0,2.0}'::real[]);
+SELECT array_dist(null::real[], '{2.0,2.0,2.0,2.0}'::real[]);
+SELECT array_dist('{1.0,1.0,1.0}'::real[], '{null,2.0,2.0,2.0}'::real[]);
+SELECT array_dist('{1.0,1.0,1.0}'::real[], null::real[]);
+
+-- sqrt((1 - 1)^2 + (1 - 1)^2 + (1 - 1)^2 + (1 - 1)^2) = 0.0 
+SELECT array_dist('{1.0,1.0,1.0,1.0}'::real[], '{1.0,1.0,1.0,1.0}'::real[]);
+
+-- sqrt((1 - 2)^2 + (1 - 2)^2 + (1 - 2)^2 + (1 - 2)^2) = 2.0 
+SELECT array_dist('{1.0,1.0,1.0,1.0}'::real[], '{2.0,2.0,2.0,2.0}'::real[]);
+
+-- distances should be symmetric
+SELECT array_dist('{2.0,2.0,2.0,2.0}'::real[], '{1.0,1.0,1.0,1.0}'::real[]);
+
+-- Trivial distance 0.1 
+SELECT array_dist('{0,0,0}'::real[], '{0,0,0.1}'::real[]);
+SELECT array_dist('{0,0,0.1}'::real[], '{0,0,0}'::real[]);
+
+/*
+ * arrays_similar function
+ */
+SET gist_isax.distance_threshold TO 0.2;
+
+-- Comparisons involving NULLs should give result unknown 
+SELECT arrays_similar('{null,1.0,1.0,1.0}'::real[], '{2.0,2.0,2.0,2.0}'::real[]);
+SELECT arrays_similar(null::real[], '{2.0,2.0,2.0,2.0}'::real[]);
+SELECT arrays_similar('{1.0,1.0,1.0,1.0}'::real[], '{null,2.0,2.0,2.0}'::real[]);
+SELECT arrays_similar('{1.0,1.0,1.0,1.0}'::real[], null::real[]);
+
+-- sqrt((1 - 2)^2 + (1 - 2)^2 + (1 - 2)^2 + (1 - 2)^2) = 2.0 
+SELECT arrays_similar('{2.0,2.0,2.0,2.0}'::real[], '{1.0,1.0,1.0,1.0}'::real[]);
+
+-- Trivial distance 0.1 
+SELECT arrays_similar('{0,0,0}'::real[], '{0,0,0.1}'::real[]);
+SELECT arrays_similar('{0,0,0.1}'::real[], '{0,0,0}'::real[]);
+
+/*
+ * '%' array similarity operator
+ */
+SET gist_isax.distance_threshold TO 0.2;
+-- Comparisons involving NULLs should give result unknown 
+SELECT '{null,1.0,1.0,1.0}'::real[] % '{2.0,2.0,2.0,2.0}'::real[];
+SELECT null::real[] % '{2.0,2.0,2.0,2.0}'::real[];
+SELECT '{1.0,1.0,1.0,1.0}'::real[] % '{null,2.0,2.0,2.0}'::real[];
+SELECT '{1.0,1.0,1.0,1.0}'::real[] % null::real[];
+
+-- sqrt((1 - 2)^2 + (1 - 2)^2 + (1 - 2)^2 + (1 - 2)^2) = 2.0 
+SELECT '{2.0,2.0,2.0,2.0}'::real[] % '{1.0,1.0,1.0,1.0}'::real[];
+
+-- Trivial distance 0.1 
+SELECT '{0,0,0}'::real[] % '{0,0,0.1}'::real[];
+SELECT '{0,0,0.1}'::real[] % '{0,0,0}'::real[];
